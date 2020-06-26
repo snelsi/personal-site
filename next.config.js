45fd1ef7514bdf02e4432338bb39bd92342b0950
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-26 22:36:36 +0300

    Fix next.config extra brackets

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
-const path = require("path");
 
-module.exports = withPlugins([
-  [optimizedImages],
-  {
-    env: {
-      MESSAGE_TO_HACKERS: "🤠 Are ya winning son?",
-    },
+module.exports = withPlugins([optimizedImages], {
+  env: {
+    MESSAGE_TO_HACKERS: "🤠 Are ya winning son?",
   },
-]);
+});
