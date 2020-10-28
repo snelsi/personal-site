cab1d8921570aeacc9dced05d56d25acb5a8d58e
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-10-28 10:52:21 +0200

    Update images deviceSizes

--- next.config.js
+++ next.config.js
@@ -5,4 +5,7 @@ module.exports = withPlugins([], {
     MESSAGE_TO_HACKERS:
       "Don't believe in yourself. Believe in me! Believe in me who believes in you!",
   },
+  images: {
+    deviceSizes: [400, 800, 1200, 1600, 2000],
+  },
 });
