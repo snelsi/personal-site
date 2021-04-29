cd85b0272ee7fc02fe39feadbc15679a7fa82462
Roman Zhuravlov <snelsithepirate@gmail.com>
2021-04-29 12:00:28 +0300

    Update explorers message

--- next.config.js
+++ next.config.js
@@ -7,22 +7,16 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-module.exports = withPlugins(
+module.exports = withPlugins([
   [
-    [
-      withMDX,
-      {
-        pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-      },
-    ],
+    withMDX,
+    {
+      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    },
   ],
   {
-    env: {
-      MESSAGE_TO_HACKERS:
-        "Don't believe in yourself. Believe in me! Believe in me who believes in you!",
-    },
     future: {
       webpack5: true,
     },
   },
-);
+]);
--- scripts/hooks/сheerExplorers.ts
+++ scripts/hooks/сheerExplorers.ts
@@ -1,5 +1,26 @@
 import * as React from "react";
 
+let styles = [
+  "background: #ceceff",
+  "border-radius: 8px",
+  "color: black",
+  "display: inline-block",
+  "font-size: 24px",
+  "font-family: 'Montserrat', monospace",
+  "font-weight: bold",
+  "padding: 4px 18px",
+].join(";");
+
 export const useCheerExplorers = () => {
-  React.useEffect(() => console.log(process.env.MESSAGE_TO_HACKERS), []);
+  React.useEffect(() => {
+    console.clear();
+    console.log("%cHello, fellow hackers! 👾", styles);
+    console.log("Got any cool ideas or a desire to work as a team?");
+    console.log(
+      "Then I'm waiting for your letter %chello@snelsi.com",
+      "text-decoration: underline;",
+    );
+    console.log("Contact me here 👉 https://snelsi.com/contact");
+    console.log("Have a good day ʕ•́ᴥ•̀ʔっ♡");
+  }, []);
 };
